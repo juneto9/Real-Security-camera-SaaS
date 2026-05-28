72e8f1248731e11fbfa9dd6037e0902831e197e0
juneto9 <34877668+juneto9@users.noreply.github.com>
2026-05-28 09:48:55 -0400

    Update app.js

--- mobile/src/app.js
+++ mobile/src/app.js
@@ -1,4 +1,4 @@
-Copyimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, ActivityIndicator, Alert, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
