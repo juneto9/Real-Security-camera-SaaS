e892b9f28106fb976e708b69980436631e321aa7
juneto9 <34877668+juneto9@users.noreply.github.com>
2026-06-02 23:44:54 -0400

    fix: add missing authenticate import to users.js

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/auth');
 const userController = require('../controllers/userController');
 const { handleValidationErrors } = require('../utils/validators');
+const { updateUsage } = require('../controllers/usagePatch');
 
 router.get('/me', userController.getMe);
 router.put('/me', userController.updateMe);
 router.put('/me/password', userController.changePassword);
-
-const { updateUsage } = require('../controllers/usagePatch');
 router.patch('/usage', authenticate, updateUsage);
 
 module.exports = router;
