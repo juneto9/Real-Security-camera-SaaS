2e4e556f95844d2c0b3358db26146244fbe32ff2
juneto9 <elioe@pokenholdingsinc.com>
2026-06-21 18:19:40 +0000

    fix: remove duplicate ScanPackage/BlePackage registration in MainApplication.kt

--- mobile/android/app/src/main/java/com/securecamera/app/MainApplication.kt
+++ mobile/android/app/src/main/java/com/securecamera/app/MainApplication.kt
@@ -23,10 +23,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-              add(ScanPackage(), BlePackage())
-              add(ScanPackage(), BlePackage())
+              add(ScanPackage())
+              add(BlePackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
